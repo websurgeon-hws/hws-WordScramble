18adedf136a84b232bbd683403f051fbaba0b44c
Peter Barclay <peter@peter-barclay.com>
2019-10-22 08:35:11 +0100

    List - Static & Dynamic

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -7,9 +7,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
+            Text("Static row 1")
+            Text("Static row 2")
+
             ForEach(0..<5) {
                 Text("Dynamic row \($0)")
             }
+
+            Text("Static row 3")
+            Text("Static row 4")
         }
 
     }
