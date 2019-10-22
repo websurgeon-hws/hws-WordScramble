856eaf64198986cf3befcf1a771a6d99f450aa70
Peter Barclay <peter@peter-barclay.com>
2019-10-22 08:37:23 +0100

    List - GroupedListStyle

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -7,22 +7,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Section(header: Text("Section 1")) {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-
-            Section(header: Text("Section 2")) {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-
-            Section(header: Text("Section 3")) {
-                Text("Static row 3")
-                Text("Static row 4")
+            ForEach(0..<5) {
+                Text("Dynamic row \($0)")
             }
         }
+        .listStyle(GroupedListStyle())
 
     }
 }
